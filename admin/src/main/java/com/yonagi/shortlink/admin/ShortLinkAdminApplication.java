44b2f39e8a9fd97e13b0b10338e73c267c965803
Yonagi <1809266700@qq.com>
2024-08-13 10:28:51 +0800

    create module: admin, gateway, shortlink proj

---
+++ admin/src/main/java/com/yonagi/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,19 @@
+package com.yonagi.shortlink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Yonagi
+ * @version 1.0
+ * @program shortlink
+ * @description
+ * @date 2024/08/13 10:03
+ */
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class);
+    }
+}
+
