f87540b99021eea6a783ce47e4723dd4c1dac13d
Yonagi <1809266700@qq.com>
2024-08-14 09:29:49 +0800

    Update member service: hide user private infomation

---
+++ admin/src/main/java/com/yonagi/shortlink/admin/common/serialize/PhoneDesensitizationSerializer.java
@@ -0,0 +1,23 @@
+package com.yonagi.shortlink.admin.common.serialize;
+
+import cn.hutool.core.util.DesensitizedUtil;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+
+/**
+ * @author Yonagi
+ * @version 1.0
+ * @program shortlink
+ * @description 手机号脱敏反序列化
+ * @date 2024/08/14 09:22
+ */
+public class PhoneDesensitizationSerializer extends JsonSerializer<String> {
+    @Override
+    public void serialize(String phone, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        String phoneDesensitization = DesensitizedUtil.mobilePhone(phone);
+        jsonGenerator.writeString(phoneDesensitization);
+    }
+}
