1c32044babecaee4f9ddc9d93c6e90cfc64eb842
Yonagi <1809266700@qq.com>
2024-08-14 13:37:55 +0800

    Update admin service: distributed lock

---
+++ admin/src/main/java/com/yonagi/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,12 @@
+package com.yonagi.shortlink.admin.common.constant;
+
+/**
+ * @author Yonagi
+ * @version 1.0
+ * @program shortlink
+ * @description Redis 常量类
+ * @date 2024/08/14 13:31
+ */
+public class RedisCacheConstant {
+    public final static String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+}
--- admin/src/main/java/com/yonagi/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/yonagi/shortlink/admin/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.yonagi.shortlink.admin.common.constant.RedisCacheConstant;
 import com.yonagi.shortlink.admin.common.convention.exception.ClientException;
 import com.yonagi.shortlink.admin.common.enums.UserErrorCodeEnum;
 import com.yonagi.shortlink.admin.dao.entity.UserDO;
@@ -13,6 +14,8 @@ import com.yonagi.shortlink.admin.dto.resp.UserRespDTO;
 import com.yonagi.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -28,6 +31,7 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
 
     private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
+    private final RedissonClient redissonClient;
 
     /**
      * 根据用户名查询用户
@@ -67,10 +71,19 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         if (hasUserName(requestParam.getUsername())) {
             throw new ClientException(UserErrorCodeEnum.USER_NAME_EXIST);
         }
-        int insert = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
-        if (insert < 1) {
-            throw new ClientException(UserErrorCodeEnum.USER_SAVE_ERROR);
+        RLock lock = redissonClient.getLock(RedisCacheConstant.LOCK_USER_REGISTER_KEY + requestParam.getUsername());
+        try {
+            if (lock.tryLock()) {
+                int insert = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+                if (insert < 1) {
+                    throw new ClientException(UserErrorCodeEnum.USER_SAVE_ERROR);
+                }
+                userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+            } else {
+                throw new ClientException(UserErrorCodeEnum.USER_NAME_EXIST);
+            }
+        } finally {
+            lock.unlock();
         }
-        userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
     }
 }
