f019e517ae3a1d43516448d2866a69582e8b8787
Yonagi <1809266700@qq.com>
2024-08-16 22:05:55 +0800

    Update project service: 数据库基本类

---
+++ project/src/main/java/com/yonagi/shortlink/project/common/database/BaseDO.java
@@ -0,0 +1,35 @@
+package com.yonagi.shortlink.project.common.database;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author Yonagi
+ * @version 1.0
+ * @program shortlink
+ * @description 持久层对象基础属性
+ * @date 2024/08/15 16:28
+ */
+@Data
+public class BaseDO {
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+    /**
+     * 删除标识 0：未删除 1：已删除
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Integer delFlag;
+}
