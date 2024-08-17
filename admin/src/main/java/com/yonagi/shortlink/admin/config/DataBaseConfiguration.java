234fe48df037f7da1a190c8bd465fbeb990b8311
Yonagi <1809266700@qq.com>
2024-08-17 12:32:29 +0800

    feature: MySQL分页查询插件

---
+++ admin/src/main/java/com/yonagi/shortlink/admin/config/DataBaseConfiguration.java
@@ -0,0 +1,30 @@
+package com.yonagi.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Yonagi
+ * @version 1.0
+ * @program shortlink
+ * @description
+ * @date 2024/08/17 12:21
+ */
+
+@Configuration
+public class DataBaseConfiguration {
+
+    /**
+     * 分页插件
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
