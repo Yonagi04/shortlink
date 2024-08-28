0edfacc5452dad1280ea56b4ac6cd8f8d2086167
Yonagi <1809266700@qq.com>
2024-08-28 10:43:53 +0800

    update project service: 新增短链接汇总统计功能

--- project/src/main/java/com/yonagi/shortlink/project/dao/mapper/ShortLinkMapper.java
+++ project/src/main/java/com/yonagi/shortlink/project/dao/mapper/ShortLinkMapper.java
@@ -2,6 +2,8 @@ package com.yonagi.shortlink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.yonagi.shortlink.project.dao.entity.ShortLinkDO;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
 
 /**
  * @author Yonagi
@@ -12,4 +14,15 @@ import com.yonagi.shortlink.project.dao.entity.ShortLinkDO;
  */
 public interface ShortLinkMapper extends BaseMapper<ShortLinkDO> {
 
+    /**
+     * 短链接访问统计自增
+     */
+    @Update("update t_link set total_pv = total_pv + #{totalPv}, total_uv = total_uv + #{totalUv}, total_uip = total_uip + #{totalUip} where gid = #{gid} and full_short_url = #{fullShortUrl}")
+    void incrementStats(
+            @Param("gid") String gid,
+            @Param("fullShortUrl") String fullShortUrl,
+            @Param("totalPv") Integer totalPv,
+            @Param("totalUv") Integer totalUv,
+            @Param("totalUip") Integer totalUip
+    );
 }
