4034fffb741a7a1de47ed471518ad9370505965c
Yonagi <1809266700@qq.com>
2024-09-01 13:27:39 +0800

    optimize admin service: 优化登录逻辑

--- admin/src/main/java/com/yonagi/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/yonagi/shortlink/admin/service/impl/UserServiceImpl.java
@@ -108,6 +108,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         }
         Map<Object, Object> hasLoginMap = stringRedisTemplate.opsForHash().entries("login_" + requestParam.getUsername());
         if (CollUtil.isNotEmpty(hasLoginMap)) {
+            stringRedisTemplate.expire("login_" + requestParam.getUsername(), 30L, TimeUnit.MINUTES);
             String token = hasLoginMap.keySet()
                     .stream()
                     .findFirst()
